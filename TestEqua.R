094beadef2c0c5d04116e98cde17a17c1f1e2e54
guillaumeFAGOT <38430601+guillaumeFAGOT@users.noreply.github.com>
2021-11-24 16:59:00 +0100

    Mise à jour des commentaires

--- TestEqua.R
+++ TestEqua.R
@@ -2,3 +2,5 @@ library(equatiomatic)
 # Regression Linéaire
 mod1 <- lm(Sepal.Length~Sepal.Width, data=iris)
 extract_eq(mod1,use_coefs = TRUE)
+# a lancer depuis une interface Rmarckdown pour utiliser le lancement par Knit (Word, ou HTML ou PDF)
+
